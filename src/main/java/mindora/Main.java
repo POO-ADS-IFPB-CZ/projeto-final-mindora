6408b936bd53dfd7892f280714b19b531bd6bc26
jotaveHub <accurategx@gmail.com>
2026-07-24 00:15:57 -0300

    style: adicionei logo do projeto na barra de titulo da janela principal

--- src/main/java/mindora/Main.java
+++ src/main/java/mindora/Main.java
@@ -4,8 +4,11 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import mindora.view.AlunoView;
+
+import java.io.InputStream;
 
 public class Main extends Application {
 
@@ -20,6 +23,14 @@ public class Main extends Application {
         tabPane.getTabs().add(tabAlunos);
 
         Scene scene = new Scene(tabPane, 800, 600);
+
+        InputStream iconStream = getClass().getResourceAsStream("/images/LogoMindora64px.jpg");
+        if (iconStream != null) {
+            primaryStage.getIcons().add(new Image(iconStream));
+        } else {
+            System.out.println("Aviso: Logo não encontrado em /images/LogoMindora64px.jpg");
+        }
+
         primaryStage.setTitle("Mindora - Sistema de Gestão");
         primaryStage.setScene(scene);
         primaryStage.show();
