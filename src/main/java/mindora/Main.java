d74c2e57ace8afd7d7e09313914cbd14d6602d27
jotaveHub <accurategx@gmail.com>
2026-07-25 17:52:08 -0300

    feat: Adicionei a aba de Profissionais ao Tabpane da interface gráfica

--- src/main/java/mindora/Main.java
+++ src/main/java/mindora/Main.java
@@ -4,11 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import mindora.view.AlunoView;
-
-import java.io.InputStream;
+import mindora.view.ProfissionalView;
 
 public class Main extends Application {
 
@@ -16,21 +14,17 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         TabPane tabPane = new TabPane();
 
-        // Aba do Aluno
+        // Aba 1: Alunos
         Tab tabAlunos = new Tab("Alunos", new AlunoView());
         tabAlunos.setClosable(false);
 
-        tabPane.getTabs().add(tabAlunos);
+        // Aba 2: Profissionais
+        Tab tabProfissionais = new Tab("Profissionais", new ProfissionalView());
+        tabProfissionais.setClosable(false);
 
-        Scene scene = new Scene(tabPane, 800, 600);
+        tabPane.getTabs().addAll(tabAlunos, tabProfissionais);
 
-        InputStream iconStream = getClass().getResourceAsStream("/images/LogoMindora64px.jpg");
-        if (iconStream != null) {
-            primaryStage.getIcons().add(new Image(iconStream));
-        } else {
-            System.out.println("Aviso: Logo não encontrado em /images/LogoMindora64px.jpg");
-        }
-
+        Scene scene = new Scene(tabPane, 850, 600);
         primaryStage.setTitle("Mindora - Sistema de Gestão");
         primaryStage.setScene(scene);
         primaryStage.show();
